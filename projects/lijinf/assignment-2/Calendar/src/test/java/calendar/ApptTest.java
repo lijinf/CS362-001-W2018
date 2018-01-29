97ae2b55b3a49cb9026c2e4dc474c657c25008f4
lijinf <lijinf@oregonstate.edu>
2018-01-28 17:10:00 -0800

    Add test to ApptTest

--- projects/lijinf/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/lijinf/assignment-2/Calendar/src/test/java/calendar/ApptTest.java
@@ -40,8 +40,139 @@ public class ApptTest {
 
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
+	 	 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 title,
+				 description);
+		 appt.setStartHour(-1);
+		 assertFalse(appt.getValid());
+		 appt.setStartHour(24);
+		 assertFalse(appt.getValid());
+		 appt.setStartHour(21);
+		 assertTrue(appt.getValid());
+
+		 appt.setStartMinute(-1);
+		 assertFalse(appt.getValid());
+		 appt.setStartMinute(80);
+		 assertFalse(appt.getValid());
+		 appt.setStartMinute(30);
+		 assertTrue(appt.getValid());
+
+		 appt.setStartDay(0);
+		 assertFalse(appt.getValid());
+		 appt.setStartDay(50);
+		 assertFalse(appt.getValid());
+		 appt.setStartDay(15);
+		 assertTrue(appt.getValid());
+
+		 appt.setStartYear(0);
+		 assertTrue(appt.getStartYear() == 0);
+		 appt.setStartYear(2018);
+		 assertTrue(appt.getStartYear() == 2018);
+
+		 appt.setStartMonth(12);
+		 assertTrue(appt.getStartMonth() == 12);
+		 appt.setStartMonth(01);
+		 assertTrue(appt.getStartMonth() == 01);
+
+		 appt.setTitle(null);
+		 assertEquals("",appt.getTitle());
+		 appt.setTitle(title);
+		 assertEquals(title,appt.getTitle());
+		 appt.setDescription(null);
+		 assertEquals("",appt.getDescription());
+		 appt.setDescription(description);
+		 assertEquals(description,appt.getDescription());
+
 	 }
+	 @Test
+	  public void test03()  throws Throwable {
+	 	 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 Appt appt = new Appt(startHour,
+		 		startMinute ,
+		 		startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		 appt.setStartHour(-1);
+		 assertEquals(-1,appt.getStartHour());
+		 assertNull(appt.toString());
+		 appt.setStartHour(21);
+		 assertEquals(21,appt.getStartHour());
+		 assertFalse(appt.toString() == null);
+	  }
+	@Test
+	public void test04()  throws Throwable {
+		int startHour=21;
+		int startMinute=30;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		int[] recurringDays = new int[0];
+		appt.setRecurrence(recurringDays, 2, 0, 0);
+		assertEquals(0, appt.getRecurIncrement());
+		assertEquals(0, appt.getRecurNumber());
+		assertEquals(2, appt.getRecurBy());
+		assertEquals(recurringDays, appt.getRecurDays());
+		assertFalse(appt.isRecurring());
+		appt.setRecurrence(null, 2, 0, 0);
+		assertEquals(0, appt.getRecurIncrement());
+		assertEquals(0, appt.getRecurNumber());
+		assertEquals(2, appt.getRecurBy());
+		assertTrue(appt.getRecurDays() != null);
+		assertFalse(appt.isRecurring());
+	}
+	@Test
+	public void test05()  throws Throwable {
+		int startHour=21;
+		int startMinute=30;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Birthday Party";
+		String description="This is my birthday party.";
+		Appt appt = new Appt(startHour,
+				startMinute ,
+				startDay ,
+				startMonth ,
+				startYear ,
+				title,
+				description);
+		Appt comAppt = new Appt(startHour-1,
+				startMinute-1 ,
+				startDay-1 ,
+				startMonth+1 ,
+				startYear-1 ,
+				title,
+				description);
+		assertEquals(3,appt.compareTo(comAppt));
+	}
 //add more unit tests as you needed
 	
 }

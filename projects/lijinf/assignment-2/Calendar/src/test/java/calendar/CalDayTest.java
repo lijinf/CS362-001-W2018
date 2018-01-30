a717f739475e980a82fcdf344c827d1a57a6b290
lijinf <lijinf@oregonstate.edu>
2018-01-29 19:27:13 -0800

    Add test cases in CalDay and TimeTable

--- projects/lijinf/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/lijinf/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -15,11 +15,47 @@ public class CalDayTest {
 
 	 @Test
 	  public void test01()  throws Throwable  {
-
+		 GregorianCalendar cal = new GregorianCalendar();
+		 CalDay calday_null = new CalDay();
+		 assertFalse(calday_null.isValid());
+		 assertNull(calday_null.iterator());
+		 CalDay calday = new CalDay(cal);
+		 assertTrue(calday.isValid());
+		 assertEquals(calday.getDay(),cal.get(cal.DAY_OF_MONTH));
+		 assertEquals(calday.getMonth(),cal.get(cal.MONTH));
+		 assertEquals(calday.getYear(),cal.get(cal.YEAR));
+		 assertEquals(0,calday.getSizeAppts());
+		 assertFalse(calday.iterator() == null);
 	 }
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
+		 GregorianCalendar cal = new GregorianCalendar();
+		 CalDay calday = new CalDay(cal);
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 "",
+				 "");
+		 appt.setStartHour(-1);
+		 calday.addAppt(appt);
+		 appt.setStartHour(21);
+		 calday.addAppt(appt);
+
+	 }
+	 @Test
+		public void test03()  throws Throwable  {
+		 GregorianCalendar cal = new GregorianCalendar();
+		 CalDay calday_null = new CalDay();
+		 calday_null.toString();
+		 CalDay calday = new CalDay(cal);
+		 calday.toString();
 	 }
 //add more unit tests as you needed	
 }
--- projects/lijinf/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
+++ projects/lijinf/assignment-2/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -17,11 +17,107 @@ public class TimeTableTest {
 
 	 @Test
 	  public void test01()  throws Throwable  {
-		 
+		 TimeTable timeTable=new TimeTable();
+		 GregorianCalendar first = new GregorianCalendar();
+		 GregorianCalendar last = new GregorianCalendar(2018,10,25);
+		 LinkedList<Appt> appts = new LinkedList<Appt>();
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 int[] recurringDays = new int[0];
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 "",
+				 "");
+		 appt.setRecurrence(recurringDays, 1, 0, 1);
+		 appts.add(appt);
+		 appt = new Appt(startHour,
+				 startMinute ,
+				 startDay + 1,
+				 startMonth + 1,
+				 startYear ,
+				 "",
+				 "");
+
+		 appt.setRecurrence(recurringDays, 1, 0, 1);
+		 appts.add(appt);
+		 appt = new Appt(-1,
+				 startMinute ,
+				 startDay + 1,
+				 startMonth + 1,
+				 startYear ,
+				 "",
+				 "");
+		 appts.add(appt);
+		 timeTable.getApptRange(appts,first,last);
+		 //timeTable.getApptRange(appts,last,first);
+
 	 }
 	 @Test
 	  public void test02()  throws Throwable  {
-		 
+		 TimeTable timeTable=new TimeTable();
+		 LinkedList<Appt> appts = new LinkedList<Appt>();
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 int[] recurringDays = new int[0];
+		 Appt appt = null;
+		 assertNull(timeTable.deleteAppt(appts, appt));//both null
+		 appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 "",
+				 "");
+		 assertNull(timeTable.deleteAppt(appts, appt));//appts == null
+		 appts.add(appt);
+		 appt = null;
+		 assertNull(timeTable.deleteAppt(appts, appt));// appt == null
+		 appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 "",
+				 "");
+		 appts.add(appt);
+		 appt.setStartHour(-1);
+		 assertNull(timeTable.deleteAppt(appts, appt));
+		 appt.setStartHour(21);
+		 appts.add(appt);
+		 appts.add(appt);
+		 assertNull(timeTable.deleteAppt(appts, appt));
 	 }
+	 @Test
+		public void test03()  throws Throwable  {
+		 TimeTable timeTable=new TimeTable();
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 Appt appt = new Appt(startHour,
+				 startMinute ,
+				 startDay ,
+				 startMonth ,
+				 startYear ,
+				 "",
+				 "");
+		 LinkedList<Appt> appts = new LinkedList<Appt>();
+		 appts.add(appt);
+		 appts.add(appt);
+		 int[] pv = new int[1];
+		 timeTable.permute(appts,pv);
+		 pv = new int[2];
+		 timeTable.permute(appts,pv);
+		}
 //add more unit tests as you needed
 }
